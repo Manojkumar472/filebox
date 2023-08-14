e07a9f0bb422854dfd56309f3a3cafa1f6f9ab8e
Manojkumar472 <121183062+Manojkumar472@users.noreply.github.com>
2023-08-14 10:42:46 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,38 @@
+
+// {
+//   '': {
+//     fileKey: '208c891c-5001-7082-96ca-ec759f6f6b74/',
+//     fileSize: 0,
+//     lastModified: 2023-08-05T20:48:57.000Z
+//   },
+//   demouser: {
+//     'subFolder':{
+//       'subFile1':{
+//         fileKey: '208c891c-5001-7082-96ca-ec759f6f6b74/demouser/itachi.png',
+//         fileSize: 596058,
+//         lastModified: 2023-08-05T20:49:32.000Z
+//       },
+//       'subFile2':{
+//         fileKey: '208c891c-5001-7082-96ca-ec759f6f6b74/demouser/itachi.png',
+//         fileSize: 596058,
+//         lastModified: 2023-08-05T20:49:32.000Z
+//       }
+//     },
+//     'itachi.png': {
+//       fileKey: '208c891c-5001-7082-96ca-ec759f6f6b74/demouser/itachi.png',
+//       fileSize: 596058,
+//       lastModified: 2023-08-05T20:49:32.000Z
+//     },
+//     'naruto.png': {
+//       fileKey: '208c891c-5001-7082-96ca-ec759f6f6b74/demouser/naruto.png',
+//       fileSize: 324259,
+//       lastModified: 2023-08-05T20:49:32.000Z
+//     },
+//     'sasuke.png': {
+//       fileKey: '208c891c-5001-7082-96ca-ec759f6f6b74/demouser/sasuke.png',
+//       fileSize: 230632,
+//       lastModified: 2023-08-05T20:49:32.000Z
+//     }
+//   }
+// }
+  
